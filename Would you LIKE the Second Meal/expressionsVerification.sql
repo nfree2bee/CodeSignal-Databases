653c32d723f0e5dad6bce7e4068dd002baf0f7da
nfree2bee <39080782+nfree2bee@users.noreply.github.com>
2018-07-30 20:31:09 +0700

    Create expressionsVerification.sql

---
+++ Would you LIKE the Second Meal/expressionsVerification.sql
@@ -0,0 +1,9 @@
+CREATE PROCEDURE expressionsVerification()
+BEGIN
+	SELECT * 
+    FROM expressions
+    WHERE (operation='+' AND c=a+b)
+        OR(operation='-' AND c=a-b)
+        OR(operation='/' AND c=a/b)
+        OR(operation='*' AND c=a*b);
+END
