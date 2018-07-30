6b67296806f9d95e90ffbe4934d5cd3e45b9ba5a
nfree2bee <39080782+nfree2bee@users.noreply.github.com>
2018-07-30 20:43:27 +0700

    Update countriesInfo.sql

--- GROUP dishes BY Type/countriesInfo.sql
+++ GROUP dishes BY Type/countriesInfo.sql
@@ -1,4 +1,4 @@
 CREATE PROCEDURE countriesInfo()
 BEGIN
-	SELECT COUNT(*) AS number, avg(population) AS average, sum(population) AS total FROM countries;
+	SELECT COUNT(*) AS number, AVG(population) AS average, SUM(population) AS total FROM countries;
 END
