58e4f0872f360f7f532a8edb5263b4dfd575e410
nfree2bee <39080782+nfree2bee@users.noreply.github.com>
2018-08-08 17:08:56 +0700

    Create soccerGameSeries.sql

---
+++ WHEN was it the CASE/soccerGameSeries.sql
@@ -0,0 +1,17 @@
+CREATE PROCEDURE soccerGameSeries()
+BEGIN
+    SELECT CASE
+        WHEN SUM(IF(fts > sts,1,0)) > SUM(IF(fts < sts,1,0)) THEN 1
+        WHEN SUM(IF(fts > sts,1,0)) < SUM(IF(fts < sts,1,0)) THEN 2
+        WHEN SUM(fts) > SUM(sts) THEN 1
+        WHEN SUM(fts) < SUM(sts) THEN 2
+        WHEN SUM(IF(mh = 2,fts,0)) > SUM(IF(mh = 1,sts,0)) THEN 1
+        WHEN SUM(IF(mh = 2,fts,0)) < SUM(IF(mh = 1,sts,0)) THEN 2
+        ELSE 0
+        END as winner
+    FROM
+    (
+        SELECT first_team_score as fts, second_team_score as sts, match_host as mh
+        FROM scores
+    ) x;
+END
