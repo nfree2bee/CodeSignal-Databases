d36f6249095c0fb3c4886702a8a4c5fb369e3a36
Harshal Sabale <30774666+harshalsabale@users.noreply.github.com>
2022-03-07 10:59:46 +0530

    Correction

--- GROUP dishes BY Type/marketReport.sql
+++ GROUP dishes BY Type/marketReport.sql
@@ -1,8 +1,10 @@
 CREATE PROCEDURE marketReport()
 BEGIN
-	SELECT country, COUNT(*) AS competitors
-    FROM foreignCompetitors
-    GROUP BY country 
-    UNION
-    SELECT 'Total:', COUNT(*) FROM foreignCompetitors;
+	(SELECT * FROM 
+	 	(SELECT country, COUNT(*) AS competitors
+        	FROM foreignCompetitors
+        	GROUP BY country 
+        	ORDER By country ASC) AS table1)
+	UNION
+		(SELECT 'Total:', COUNT(*) FROM foreignCompetitors);
 END
